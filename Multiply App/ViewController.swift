d5609563d07422c0ff8ab475abf67f1975a81fe2
VMassarelli <student@vmassarelli.local>
2016-01-08 11:28:03 -0600

    intial commit

--- Multiply App/ViewController.swift
+++ Multiply App/ViewController.swift
@@ -8,18 +8,107 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate
+{
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var labelAnswer: UILabel!
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var SegmentController: UISegmentedControl!
+    @IBOutlet weak var labelSign: UILabel!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if SegmentController.selectedSegmentIndex == 0
+        {
+            labelSign.text = "-"
+        }
+        if SegmentController.selectedSegmentIndex == 1
+        {
+            labelSign.text = "+"
+        }
+        if SegmentController.selectedSegmentIndex == 2
+        {
+            labelSign.text = "X"
+        }
+        if SegmentController.selectedSegmentIndex == 3
+        {
+            labelSign.text = "/"
+        }
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func SegControl(sender: AnyObject)
+    {
+        if SegmentController.selectedSegmentIndex == 0
+        {
+            labelSign.text = "-"
+        }
+        if SegmentController.selectedSegmentIndex == 1
+        {
+            labelSign.text = "+"
+        }
+        if SegmentController.selectedSegmentIndex == 2
+        {
+            labelSign.text = "x"
+        }
+        if SegmentController.selectedSegmentIndex == 3
+        {
+            labelSign.text = "/"
+        }
     }
+    @IBAction func calcButton(sender: AnyObject)
+    {
+    
+        let multiply1 = (textField1.text! as NSString).doubleValue
+        let multiply2 = (textField2.text! as NSString).doubleValue
+        var finalAnswer = 0.00
+       
+        if SegmentController.selectedSegmentIndex == 0
+        {
+            finalAnswer = multiply1 - multiply2
+            labelAnswer.text = "\(finalAnswer)"
+        }
+        if SegmentController.selectedSegmentIndex == 1
+        {
+            finalAnswer = multiply1 + multiply2
+            labelAnswer.text = "\(finalAnswer)"
 
+        }
+        if SegmentController.selectedSegmentIndex == 2
+        {
+             finalAnswer = multiply1 * multiply2
+            labelAnswer.text = "\(finalAnswer)"
 
+        }
+        if SegmentController.selectedSegmentIndex == 3
+        {
+            finalAnswer = multiply1 / multiply2
+            labelAnswer.text = "\(finalAnswer)"
+
+        }
+        if(finalAnswer == 64)
+        {
+            ImageView.image = UIImage(named: "mario64sw.jpg")
+        }
+        else if (finalAnswer % 2 != 0)
+        {
+            ImageView.image = UIImage(named: "jesus_is_cool")
+        }
+        else
+        {
+            ImageView.image = UIImage(named: "funny-this-is-sparta")
+        }
+        if(finalAnswer % 1 == 0)
+        {
+            let newAnswer = Int(finalAnswer)
+            labelAnswer.text = "\(newAnswer)"
+        }
+    }
 }
+
+
+
+
 
